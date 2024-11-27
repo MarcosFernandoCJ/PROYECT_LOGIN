a614174a9ff1b69b9493b523118de952df792d55
Marcos Chocce <fernando.chocce@tecsup.edu.pe>
2024-11-26 22:14:36 -0500

    bakcken login

---
+++ src/main/java/com/tecup/backend/models/ERole.java
@@ -0,0 +1,8 @@
+package com.tecup.backend.models;
+
+public enum ERole {
+    ROLE_USER,
+    ROLE_JURADO,
+    ROLE_ADMIN,
+    ROLE_ORGANIZADOR
+}
---
+++ src/main/java/com/tecup/backend/repository/RoleRepository.java
@@ -0,0 +1,13 @@
+package com.tecup.backend.repository;
+
+import com.tecup.backend.models.ERole;
+import com.tecup.backend.models.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Optional<Role> findByName(ERole name);
+}
